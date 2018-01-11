1f1b013037cf73515fe0b8a6ee0d44df6be0707d
hsunyuan <cool8676@gmail.com>
2018-01-11 16:26:19 +0800

    Complete text gui

--- src/gui.java
+++ src/gui.java
@@ -6,6 +6,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.swing.JTextArea;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.PrintStream;
+import java.util.Date;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.text.BadLocationException;
+
 
 interface Action {
 
@@ -221,6 +244,7 @@ class Person extends Thread {
          */
         System.out.println("a person : " + Thread.currentThread().getName()
                 + " is swiming" + "(d)");
+
         this.setPriority(MAX_PRIORITY);//depature is given a high priority
 
         /*
@@ -249,7 +273,7 @@ class Person extends Thread {
          * if all people has depatured , stop viewer
          */
         if (viewer.population == newPool.depature) {
-            System.out.println("has done");
+            System.out.println("\n------------------------------\ndone");
             super.stop();//stop viewer
         }
         this.stop();//stops thread
@@ -358,27 +382,106 @@ class update extends Thread {
     }
 }
 
-public class gui {
-    private JButton startButton;
-    private JPanel panelMain = new JPanel();
+
+
+public class gui extends JFrame{
+    private PrintStream standardOut;
     public static int go = 0;
-    public gui() {
-        startButton.addActionListener(new ActionListener() {
+    private JButton buttonStart = new JButton("Start");
+    public JTextArea textArea;
+    private JButton buttonClear = new JButton("Clear");
+    private JPanel panelMain = new JPanel();
+    public gui(){
+        super("Os");
+
+        textArea = new JTextArea(50, 10);
+        textArea.setEditable(false);
+        PrintStream printStream = new PrintStream(new CustomOutputStream(textArea));
+
+        // keeps reference of standard output stream
+        standardOut = System.out;
+
+        // re-assigns standard output stream and error output stream
+        System.setOut(printStream);
+        System.setErr(printStream);
+
+        // creates the GUI
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(10, 10, 10, 10);
+        constraints.anchor = GridBagConstraints.WEST;
+
+        add(buttonStart, constraints);
+
+        constraints.gridx = 1;
+        add(buttonClear, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weightx = 1.0;
+        constraints.weighty = 1.0;
+
+        add(new JScrollPane(textArea), constraints);
+
+        // adds event handler for button Start
+        buttonStart.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent evt) {
                 go = 1;
+
             }
         });
+
+        // adds event handler for button Clear
+        buttonClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                // clears the text area
+                try {
+                    textArea.getDocument().remove(0,
+                            textArea.getDocument().getLength());
+                    standardOut.println("Text area cleared");
+                } catch (BadLocationException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(480, 320);
+        setLocationRelativeTo(null);    // centers on screen
     }
 
-    public static void main(String[] args)throws InterruptedException{
-        JFrame frame = new JFrame("swimPool");
-        frame.setContentPane(new gui().panelMain);
-        frame.add(new gui().startButton);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
 
-        frame.setVisible(true);
+
+
+//        buttonStart.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                go = 1;
+//            }
+//        });
+
+
+    public static void main(String[] args)throws InterruptedException{
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new gui().setVisible(true);
+            }
+        });
+//
+//        JFrame frame = new JFrame("swimPool");
+//        frame.setContentPane(new gui().panelMain);
+//        frame.add(new gui().startButton);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
 
         while(go == 0){
             try {
